7fc742999db61fc0e1c9fba922993426533143a3
Oskar Goczał <viventermail@gmail.com>
2023-07-02 15:35:32 +0200

    dodanie timera po części

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 import {
   StyledTimer,
   StartButton,
@@ -17,15 +18,39 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function Timer() {
+  const [paused, setPaused] = useState(true);
+  const [countDown, setCountDown] = useState(30 * 60);
+  const [timer, setTimer] = useState("30:00");
+
+  useEffect(() => {
+    if (!paused) {
+      const interval = setInterval(() => {
+        setCountDown((prevCount) => prevCount - 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [paused]);
+
+  useEffect(() => {
+    const formattedDate = moment.unix(countDown).format("mm:ss");
+    setTimer(formattedDate);
+  }, [countDown]);
+
   return (
     <StyledTimer>
-      <time>30:00</time>
-      <StartButton>
-        <FontAwesomeIcon icon={faPlay} />
-      </StartButton>
-      <PauseButton>
-        <FontAwesomeIcon icon={faPause} />
-      </PauseButton>
+      <time>{timer}</time>
+      {paused ? (
+        <StartButton>
+          <FontAwesomeIcon icon={faPlay} onClick={() => setPaused(false)} />
+        </StartButton>
+      ) : (
+        <PauseButton>
+          <FontAwesomeIcon icon={faPause} onClick={() => setPaused(true)} />
+        </PauseButton>
+      )}
       <FinishButton>
         <FontAwesomeIcon icon={faStop} />
       </FinishButton>
